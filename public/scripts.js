84c50bbeebf711936b8bbde40d3b4c773cc021ed
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-01 03:56:20 +0530

    Added

--- public/scripts.js
+++ public/scripts.js
@@ -1,22 +1,22 @@
-const searchElement = document.querySelector('[data-city-search]');
-const searchBox = new google.maps.places.SearchBox(searchElement);
-
-searchBox.addListener('places_changed', ()=>{
-    const place = searchBox.getPlaces()[0]
-    if(place == null) return;
-    const latitude = place.geometry.lat();
-    const longitude = place.geometry.lng();
-
-    fetch('/weather', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json',
-        accept: "application/json"
-    }, 
+const searchElement = document.querySelector('[data-city-search]')
+const searchBox = new google.maps.places.SearchBox(searchElement)
+searchBox.addListener('places_changed', () => {
+  const place = searchBox.getPlaces()[0]
+  if (place == null) return
+  const latitude = place.geometry.location.lat()
+  const longitude = place.geometry.location.lng()
+  fetch('/weather', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
     body: JSON.stringify({
-        latitude, latitude,
-        longitude, longitude
+      latitude: latitude,
+      longitude: longitude
     })
-    }).then(res => res.json()).then(data =>{
-        setWeatherData(data, place.formatted_address)
-    })
+  }).then(res => res.json()).then(data => {
+      console.log(data);
+    // setWeatherData(data, place.formatted_address)
+  })
 })
--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
 
 //setting up the express server
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/weather', (req, res)=>{
-
+    const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`
+    // console.log(req.body);
+    axios({
+        url: url,
+        responseType: 'json'
+    }).then(data => res.json(data.data.currently))
 })
 
 const port = process.env.PORT || 4321;
