d4f9ad2b2345d870c1c9811ee22b17ae6d95362f
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-01 12:02:14 +0530

    scripts

--- public/scripts.js
+++ public/scripts.js
@@ -16,7 +16,25 @@ searchBox.addListener('places_changed', () => {
       longitude: longitude
     })
   }).then(res => res.json()).then(data => {
-      console.log(data);
-    // setWeatherData(data, place.formatted_address)
+    setWeatherData(data, place.formatted_address)
   })
 })
+
+const icon = new Skycons({ color: '#222' })
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const temperatureElement = document.querySelector('[data-temperature]')
+const precipitationElement = document.querySelector('[data-precipitation]')
+const windElement = document.querySelector('[data-wind]')
+icon.set('icon', 'clear-day')
+icon.play()
+
+function setWeatherData(data, place) {
+  locationElement.textContent = place
+  statusElement.textContent = data.summary
+  temperatureElement.textContent = data.temperature
+  precipitationElement.textContent = `${data.precipProbability * 100}%`
+  windElement.textContent = data.windSpeed
+  icon.set('icon', data.icon)
+  icon.play()
+}
